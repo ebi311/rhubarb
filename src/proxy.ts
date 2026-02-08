3f7e1cca99aeb1131ffab14fdbd354cdb9851b09
EBIHARA <ebi311@gmail.com>
2026-02-08 23:54:49 +0000

    verup: Next.js 16 で廃止となる middleware から proxy へと変換した

---
+++ src/proxy.ts
@@ -0,0 +1,53 @@
+import { NextResponse, type NextRequest } from 'next/server';
+
+export async function proxy(request: NextRequest) {
+	const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
+	const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'nonce-${nonce}';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+	// Replace newline characters and spaces
+	const contentSecurityPolicyHeaderValue = cspHeader
+		.replace(/\s{2,}/g, ' ')
+		.trim();
+
+	const requestHeaders = new Headers(request.headers);
+	requestHeaders.set('x-nonce', nonce);
+
+	requestHeaders.set(
+		'Content-Security-Policy',
+		contentSecurityPolicyHeaderValue,
+	);
+
+	const response = await NextResponse.next({
+		request: {
+			headers: requestHeaders,
+		},
+	});
+	response.headers.set(
+		'Content-Security-Policy',
+		contentSecurityPolicyHeaderValue,
+	);
+	return response;
+}
+
+export const config = {
+	matcher: [
+		/*
+		 * Match all request paths except for the ones starting with:
+		 * - _next/static (static files)
+		 * - _next/image (image optimization files)
+		 * - favicon.ico (favicon file)
+		 * Feel free to modify this pattern to include more paths.
+		 */
+		'/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+	],
+};
